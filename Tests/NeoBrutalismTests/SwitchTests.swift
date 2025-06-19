3bf016a04ba367e5f65a78ee3c122cf66a088902
Kunal Kamble <kunalgk1999@gmail.com>
2025-06-20 00:10:39 +0530

    SwitchTests

--- Tests/NeoBrutalismTests/SwitchTests.swift
+++ Tests/NeoBrutalismTests/SwitchTests.swift
@@ -1,73 +1,73 @@
-import NeoBrutalism
-import SnapshotTestingMacros
-import SwiftUI
-import Testing
+// import NeoBrutalism
+// import SnapshotTestingMacros
+// import SwiftUI
+// import Testing
 
-// TODO: Output image is not correct for switch
-// https://github.com/adammcarter/swift-snapshot-testing-macros/issues/43
-@Suite @SnapshotSuite @MainActor
-struct SwitchTests {
-    // MARK: - Switch
+// // TODO: Output image is not correct for switch
+// // https://github.com/adammcarter/swift-snapshot-testing-macros/issues/43
+// @Suite @SnapshotSuite @MainActor
+// struct SwitchTests {
+//     // MARK: - Switch
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switch_on() -> some View {
-        Toggle(isOn: .constant(true)) {}
-            .toggleStyle(.neoBrutalismSwitch)
-    }
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switch_on() -> some View {
+//         Toggle(isOn: .constant(true)) {}
+//             .toggleStyle(.neoBrutalismSwitch)
+//     }
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switch_on_disabled() -> some View {
-        Toggle(isOn: .constant(true)) {}
-            .disabled(true)
-            .toggleStyle(.neoBrutalismSwitch)
-    }
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switch_on_disabled() -> some View {
+//         Toggle(isOn: .constant(true)) {}
+//             .disabled(true)
+//             .toggleStyle(.neoBrutalismSwitch)
+//     }
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switch_off() -> some View {
-        Toggle(isOn: .constant(false)) {}
-            .toggleStyle(.neoBrutalismSwitch)
-    }
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switch_off() -> some View {
+//         Toggle(isOn: .constant(false)) {}
+//             .toggleStyle(.neoBrutalismSwitch)
+//     }
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switch_off_disabled() -> some View {
-        Toggle(isOn: .constant(false)) {}
-            .disabled(true)
-            .toggleStyle(.neoBrutalismSwitch)
-    }
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switch_off_disabled() -> some View {
+//         Toggle(isOn: .constant(false)) {}
+//             .disabled(true)
+//             .toggleStyle(.neoBrutalismSwitch)
+//     }
 
-    // MARK: - Switch with label
+//     // MARK: - Switch with label
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switchWithLabel_on() -> some View {
-        Toggle(isOn: .constant(true)) { Text("Switch") }
-            .toggleStyle(.neoBrutalismSwitch)
-    }
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switchWithLabel_on() -> some View {
+//         Toggle(isOn: .constant(true)) { Text("Switch") }
+//             .toggleStyle(.neoBrutalismSwitch)
+//     }
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switchWithLabel_on_disabled() -> some View {
-        Toggle(isOn: .constant(true)) { Text("Switch") }
-            .disabled(true)
-            .toggleStyle(.nbSwitch)
-    }
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switchWithLabel_on_disabled() -> some View {
+//         Toggle(isOn: .constant(true)) { Text("Switch") }
+//             .disabled(true)
+//             .toggleStyle(.nbSwitch)
+//     }
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switchWithLabel_off() -> some View {
-        Toggle(isOn: .constant(false)) { Text("Switch") }
-            .toggleStyle(.nbSwitch)
-    }
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switchWithLabel_off() -> some View {
+//         Toggle(isOn: .constant(false)) { Text("Switch") }
+//             .toggleStyle(.nbSwitch)
+//     }
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switchWithLabel_off_disabled() -> some View {
-        Toggle(isOn: .constant(false)) { Text("Switch") }
-            .disabled(true)
-            .toggleStyle(.nbSwitch)
-    }
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switchWithLabel_off_disabled() -> some View {
+//         Toggle(isOn: .constant(false)) { Text("Switch") }
+//             .disabled(true)
+//             .toggleStyle(.nbSwitch)
+//     }
 
-    // MARK: - Switch with large label
+//     // MARK: - Switch with large label
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
-    func switchWithLargeLabel() -> some View {
-        Toggle(isOn: .constant(true)) { Text("Switch").font(.largeTitle) }
-            .toggleStyle(.nbSwitch)
-    }
-}
+//     @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+//     func switchWithLargeLabel() -> some View {
+//         Toggle(isOn: .constant(true)) { Text("Switch").font(.largeTitle) }
+//             .toggleStyle(.nbSwitch)
+//     }
+// }
