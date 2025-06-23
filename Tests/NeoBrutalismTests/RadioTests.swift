ee2f825ef2141126038b37a2044537ca96a62369
Kunal Kamble <kunalgk1999@gmail.com>
2025-06-23 23:51:18 +0530

    minimum

--- Tests/NeoBrutalismTests/RadioTests.swift
+++ Tests/NeoBrutalismTests/RadioTests.swift
@@ -7,7 +7,7 @@ import Testing
 struct RadioTests {
     // MARK: - Radio Group
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+    @SnapshotTest(.padding(4.0), .sizes(.minimum), .backgroundColor(NBTheme.default.background))
     func radio_singleSelection_firstSelected() -> some View {
         return NBRadioGroup(value: .constant(0)) {
             NBRadioItem(value: 0) {
@@ -19,7 +19,7 @@ struct RadioTests {
         }.fixedSize()
     }
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+    @SnapshotTest(.padding(4.0), .sizes(.minimum), .backgroundColor(NBTheme.default.background))
     func radio_singleSelection_secondSelected() -> some View {
         return NBRadioGroup(value: .constant(1)) {
             NBRadioItem(value: 0) {
@@ -33,7 +33,7 @@ struct RadioTests {
 
     // MARK: - Radio Group with Label
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+    @SnapshotTest(.padding(4.0), .sizes(.minimum), .backgroundColor(NBTheme.default.background))
     func radio_withLabel() -> some View {
         return NBRadioGroup(value: .constant(2)) {
             Text("Choose an option")
@@ -52,7 +52,7 @@ struct RadioTests {
 
     // MARK: - Radio Item Standalone
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+    @SnapshotTest(.padding(4.0), .sizes(.minimum), .backgroundColor(NBTheme.default.background))
     func radioItem_selected() -> some View {
         NBRadioItem(value: 0) {
             Text("Standalone")
@@ -61,7 +61,7 @@ struct RadioTests {
         .environment(\.nbSelectedRadioItemValue, 0)
     }
 
-    @SnapshotTest(.padding(4.0), .backgroundColor(NBTheme.default.background))
+    @SnapshotTest(.padding(4.0), .sizes(.minimum), .backgroundColor(NBTheme.default.background))
     func radioItem_unselected() -> some View {
         NBRadioItem(value: 1) {
             Text("Standalone")
