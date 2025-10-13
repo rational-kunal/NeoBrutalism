1517c93dc0fac990e5b57fc2ca140a57c9e3492e
Indrajit Chakrabarty <indyfromoz@gmail.com>
2025-10-13 12:03:29 +1100

    feat: Add new theme presets and reorganize theme extensions
    - Relocated Theme+.swift from Example/Sources to Sources/NeoBrutalism/Common
    - Added multiple new NBTheme color presets.
    - Updated ContentView to use NBTheme.default directly, removing custom theme initialization.

--- Example/Sources/ContentView.swift
+++ Example/Sources/ContentView.swift
@@ -345,12 +345,8 @@ struct FlatCardExampleView: View {
 
 struct ContentView: View {
     @State var colorSceme: ColorScheme = .light
-    @State var theme = NBTheme.default.updateBy(
-        main: Color(light: .rgb(1.0, 0.42, 0.42), dark: .rgb(1.0, 0.42, 0.42)),
-        bw: Color(light: .rgb(1.0, 1.0, 1.0), dark: .rgb(0.129, 0.129, 0.129)),
-        background: Color(light: .rgb(0.988, 0.843, 0.843), dark: .rgb(0.153, 0.161, 0.2))
-    )
-
+    @State var theme = NBTheme.default
+    
 //    var body: some View {
 //        ZStack {
 //            theme.background
